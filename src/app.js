537b130c74d6ae873b0f411f92a13a4b68793efc
stephmailbox6-stack <stephmailbox6@gmail.com>
2025-12-24 09:36:20 +0000

    Added more weather elements

--- src/app.js
+++ src/app.js
@@ -2,9 +2,42 @@ function updateTenkiYoho(response) {
   let temperatureElement = document.querySelector("#tenki-temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#tenki-city");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  console.log(response.data);
 
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = refreshDate(date);
+}
+
+function refreshDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (hours < 10 || minutes < 10) {
+    hours = `0${hours}`;
+    minutes = `${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
